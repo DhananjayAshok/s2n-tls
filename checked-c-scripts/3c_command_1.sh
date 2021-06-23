0366837265617b7a8a55fa6c8e6468e452cb56c4
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2021-06-23 13:05:47 -0400

    Added First 3C command

---
+++ checked-c-scripts/3c_command_1.sh
@@ -0,0 +1,12 @@
+3c --addcr --alltypes --warn-root-cause --output-dir=out -p=build/compile_commands.json error/s2n_errno.c \
+stuffer/s2n_stuffer_base64.c \
+stuffer/s2n_stuffer_file.c \
+stuffer/s2n_stuffer_network_order.c \
+stuffer/s2n_stuffer_pem.c \
+stuffer/s2n_stuffer_text.c \
+stuffer/s2n_stuffer.c \
+utils/s2n_safety.c \
+utils/s2n_blob.c \
+utils/s2n_mem.c \
+--wildptrstats-output=checked-c-scripts/3c_command_1_wildptrstats.json \
+--perptrstats-output=checked-c-scripts/3c_command_1_perptrstats.json
