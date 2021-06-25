672ea53c885b547d32eb797bb6e95f27c074edb7
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2021-06-25 17:05:49 -0400

    Removed Ninja Command

--- checked-c-scripts/build_s2n.sh
+++ checked-c-scripts/build_s2n.sh
@@ -2,7 +2,7 @@ echo "This Script Should Be Called From Root Directory, Not s2n-tls/checked-c-sc
 #rm -rf build
 
 CC=/Users/dhananas/CheckedC/build/bin/clang cmake . \
-    -Bbuild -GNinja \
+    #-Bbuild -GNinja \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_PREFIX_PATH=$(dirname $(dirname $(brew list openssl@1.1|grep libcrypto.dylib))) \
     -DCMAKE_C_FLAGS="-D_GNU_SOURCE" \
