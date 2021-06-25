69d62cd8fa925c29c5d0743d19713a8e5d2a45b3
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2021-06-25 19:41:50 -0400

    Fixed Script issue

--- checked-c-scripts/build_s2n.sh
+++ checked-c-scripts/build_s2n.sh
@@ -1,12 +1,12 @@
 echo "This Script Should Be Called From Root Directory, Not s2n-tls/checked-c-scripts"
 #rm -rf build
 
-CC=$(LLVM_OBJ)/bin/clang cmake . -Bbuild \
+CC=/Users/dhananas/CheckedC/build/bin/clang cmake . -Bbuild \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_PREFIX_PATH=$(dirname $(dirname $(brew list openssl@1.1|grep libcrypto.dylib))) \
     -DCMAKE_C_FLAGS="-D_GNU_SOURCE" \
-    -DBUILD_TESTING=OFF
-    #-GNinja \
+    -DBUILD_TESTING=OFF #\
+    #-GNinja
 cmake --build ./build -j 12
 echo "Finished generating compilation database for s2n-tls. The compile_commands.json file should be at build/compile_commands.json"
 
