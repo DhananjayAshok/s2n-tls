735727027b2a0b6ee0944794682e0da6018fa77b
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2021-06-25 17:05:33 -0400

    Made space separation

--- checked-c-scripts/build_s2n.sh
+++ checked-c-scripts/build_s2n.sh
@@ -1,11 +1,12 @@
 echo "This Script Should Be Called From Root Directory, Not s2n-tls/checked-c-scripts"
 #rm -rf build
 
-CC=/Users/dhananas/CheckedC/build/bin/clang cmake . -Bbuild -GNinja \
+CC=/Users/dhananas/CheckedC/build/bin/clang cmake . \
+    -Bbuild -GNinja \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_PREFIX_PATH=$(dirname $(dirname $(brew list openssl@1.1|grep libcrypto.dylib))) \
     -DCMAKE_C_FLAGS="-D_GNU_SOURCE" \
     -DBUILD_TESTING=OFF
-#cmake --build ./build -j $(nproc)
+#cmake --build ./build -j 12
 echo "Finished generating compilation database for s2n-tls. The compile_commands.json file should be at build/compile_commands.json"
 
