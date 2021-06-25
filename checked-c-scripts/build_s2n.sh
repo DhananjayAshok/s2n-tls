a7ae15ca99b7b94763c41bfe750a13517f4a9317
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2021-06-25 16:07:24 -0400

    Updated Build Command

--- checked-c-scripts/build_s2n.sh
+++ checked-c-scripts/build_s2n.sh
@@ -4,7 +4,8 @@ rm -rf build
 CC=/Users/dhananas/CheckedC/build/bin/clang cmake . -Bbuild -GNinja \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_PREFIX_PATH=$(dirname $(dirname $(brew list openssl@1.1|grep libcrypto.dylib))) \
-    -DCMAKE_C_FLAGS="-D_GNU_SOURCE"
+    -DCMAKE_C_FLAGS="-D_GNU_SOURCE" \
+    -DBUILD_TESTING=OFF
 cmake --build ./build -j $(nproc)
 echo "Finished generating compilation database for s2n-tls. The compile_commands.json file should be at build/compile_commands.json"
 
