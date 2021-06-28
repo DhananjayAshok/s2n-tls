7f74610b79bfe11740465324eaeb005d4a79a90a
DhananjayAshok <dhananjay.ashok99@gmail.com>
2021-06-28 11:20:27 -0400

    Fixed cmake build commands

--- checked-c-scripts/build_s2n.sh
+++ checked-c-scripts/build_s2n.sh
@@ -7,6 +7,6 @@ CC=/Users/dhananas/CheckedC/build/bin/clang cmake . -Bbuild \
     -DCMAKE_C_FLAGS="-D_GNU_SOURCE" \
     -DBUILD_TESTING=OFF #\
     #-GNinja
-cmake --build ./build -j 12
+cmake --build ./build -- -j12
 echo "Finished generating compilation database for s2n-tls. The compile_commands.json file should be at build/compile_commands.json"
 
--- checked-c-scripts/linux_build_s2n.sh
+++ checked-c-scripts/linux_build_s2n.sh
@@ -1,12 +1,11 @@
 echo "This Script Should Be Called From Root Directory, Not s2n-tls/checked-c-scripts"
 #rm -rf build
 
-CC=$(LLVM_OBJ)/bin/clang cmake . -Bbuild \
+CC=/home/dhananjay/CheckedC/build/bin/clang cmake . -Bbuild \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_PREFIX_PATH=$(dirname $(dirname $(brew list openssl@1.1|grep libcrypto.dylib))) \
     -DCMAKE_C_FLAGS="-D_GNU_SOURCE" \
-    -DBUILD_TESTING=OFF
-    -GNinja \
-cmake --build ./build -j 12
+    -DBUILD_TESTING=OFF \
+    -GNinja
+cmake --build ./build -- -j12
 echo "Finished generating compilation database for s2n-tls. The compile_commands.json file should be at build/compile_commands.json"
 
